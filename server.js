49fe10b1ad6d6df61a7c3b263440a285e45a88ed
LG29878 <grajalesliliana@johndeere.com>
2021-01-27 20:57:22 -0500

    Commit 4 - index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const pokemon =require("./models/data.js");
 
+//MIDDLEWARE STARTS
+//Note that the order of the middleware apps matter, since they have dependencies
+
+
+//MIDDLEWARE ENDS
+
 app.get('/pokemon',(req,res)=>{
      res.send(pokemon)
 });
 
 app.get('/pokemon/:index',(req,res)=>{
-    res.send(pokemon[req.params.index])
+    // res.send(pokemon[req.params.index])
+    res.render("index.ejs",{pokes:pokemon})
 });
 
 app.listen(3000,()=>{
